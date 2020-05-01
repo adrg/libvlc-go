e396b824024b4d6f0f558a91081e55f5f6e9447c
Adrian-George Bostan <adrg@epistack.com>
2020-05-01 14:55:12 +0300

    Add bindings for setting application metadata in v2

--- v2/vlc.go
+++ v2/vlc.go
@@ -72,3 +72,50 @@ func Release() error {
 func Version() VersionInfo {
 	return moduleVersion
 }
+
+// SetAppName sets the human-readable application name and the HTTP user agent.
+// The specified user agent is used when a protocol requires it.
+func SetAppName(name, userAgent string) error {
+	if err := inst.assertInit(); err != nil {
+		return err
+	}
+
+	cName, cUserAgent := C.CString(name), C.CString(userAgent)
+	C.libvlc_set_user_agent(inst.handle, cName, cUserAgent)
+
+	C.free(unsafe.Pointer(cName))
+	C.free(unsafe.Pointer(cUserAgent))
+	return nil
+}
+
+// SetAppID sets metadata for identifying the application.
+func SetAppID(id, version, icon string) error {
+	if err := inst.assertInit(); err != nil {
+		return err
+	}
+
+	cID, cVersion, cIcon := C.CString(id), C.CString(version), C.CString(icon)
+	C.libvlc_set_app_id(inst.handle, cID, cVersion, cIcon)
+
+	C.free(unsafe.Pointer(cID))
+	C.free(unsafe.Pointer(cVersion))
+	C.free(unsafe.Pointer(cIcon))
+	return nil
+}
+
+// StartUI attempts to start a user interface for the libVLC instance. Pass an
+// empty string as the name parameter in order to start the default interface.
+func StartUI(name string) error {
+	if err := inst.assertInit(); err != nil {
+		return err
+	}
+
+	cName := C.CString(name)
+	defer C.free(unsafe.Pointer(cName))
+
+	if C.libvlc_add_intf(inst.handle, cName) < 0 {
+		return getError()
+	}
+
+	return nil
+}
