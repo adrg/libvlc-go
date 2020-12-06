0724378fe45519e3d7ea09c01c46d3806d600299
Adrian-George Bostan <adrg@epistack.com>
2020-12-06 11:55:21 +0200

    Add MediaTrack and related structs

--- v3/errors.go
+++ v3/errors.go
@@ -19,17 +19,18 @@ var (
 
 // Media errors.
 var (
-	ErrMediaCreate             = errors.New("could not create media")
-	ErrMediaNotFound           = errors.New("could not find media")
-	ErrMediaNotInitialized     = errors.New("media not initialized")
-	ErrMediaListCreate         = errors.New("could not create media list")
-	ErrMediaListNotFound       = errors.New("could not find media list")
-	ErrMediaListNotInitialized = errors.New("media list not initialized")
-	ErrMissingMediaStats       = errors.New("could not get media statistics")
-	ErrInvalidMediaStats       = errors.New("invalid media statistics")
-	ErrMissingMediaLocation    = errors.New("could not get media location")
-	ErrMediaMetaSave           = errors.New("could not save media metadata")
-	ErrMediaParse              = errors.New("could not parse media")
+	ErrMediaCreate              = errors.New("could not create media")
+	ErrMediaNotFound            = errors.New("could not find media")
+	ErrMediaNotInitialized      = errors.New("media not initialized")
+	ErrMediaListCreate          = errors.New("could not create media list")
+	ErrMediaListNotFound        = errors.New("could not find media list")
+	ErrMediaListNotInitialized  = errors.New("media list not initialized")
+	ErrMissingMediaStats        = errors.New("could not get media statistics")
+	ErrInvalidMediaStats        = errors.New("invalid media statistics")
+	ErrMissingMediaLocation     = errors.New("could not get media location")
+	ErrMediaMetaSave            = errors.New("could not save media metadata")
+	ErrMediaParse               = errors.New("could not parse media")
+	ErrMediaTrackNotInitialized = errors.New("media track not initialized")
 )
 
 // Event manager errors.
