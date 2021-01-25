290ec51e320ad653220dd8bf0f8dc1c09192aee6
Adrian-George Bostan <adrg@epistack.com>
2021-01-25 13:42:10 +0200

    Add Player.LoadMediaFromReadSeeker method

--- v3/player.go
+++ v3/player.go
@@ -5,6 +5,7 @@ package vlc
 // #include <stdlib.h>
 import "C"
 import (
+	"io"
 	"unsafe"
 )
 
@@ -277,16 +278,32 @@ func (p *Player) SetMedia(m *Media) error {
 	return p.setMedia(m)
 }
 
-// LoadMediaFromPath loads the media from the specified path and adds it as the
-// current media of the player.
+// LoadMediaFromPath loads the media from the specified path and sets it as
+// the current media of the player.
 func (p *Player) LoadMediaFromPath(path string) (*Media, error) {
 	return p.loadMedia(path, true)
 }
 
-// LoadMediaFromURL loads the media from the specified URL and adds it as the
-// current media of the player.
+// LoadMediaFromURL loads the media from the specified URL and sets it as
+// the current media of the player.
 func (p *Player) LoadMediaFromURL(url string) (*Media, error) {
 	return p.loadMedia(url, false)
+}
+
+// LoadMediaFromReadSeeker loads the media from the provided read seeker
+// and sets it as the current media of the player.
+func (p *Player) LoadMediaFromReadSeeker(r io.ReadSeeker) (*Media, error) {
+	m, err := NewMediaFromReadSeeker(r)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = p.setMedia(m); err != nil {
+		_ = m.Release()
+		return nil, err
+	}
+
+	return m, nil
 }
 
 // SetAudioOutput selects an audio output module.
@@ -569,7 +586,7 @@ func (p *Player) loadMedia(path string, local bool) (*Media, error) {
 	}
 
 	if err = p.setMedia(m); err != nil {
-		m.Release()
+		_ = m.Release()
 		return nil, err
 	}
 
